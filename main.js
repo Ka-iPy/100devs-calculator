647ffc419d4c402a4e6d42d0852704dffec98efc
Ka-iPy <kaighost65@gmail.com>
2025-10-25 20:49:40 +0100

    My attempt at the calculator

---
+++ main.js
@@ -0,0 +1,23 @@
+const screenEl = document.querySelector('span');         // or better: document.getElementById('screen')
+const equalsBtn = document.getElementById('equals');     // <button id="equals">=</button>
+const btns = document.getElementsByClassName('btn');              // <button id="btn">1</button>
+let result = false
+// attach handlers (make sure elements exist)
+equalsBtn.addEventListener('click', calculate);
+for (let btn of btns) {
+    
+  btn.addEventListener('click', () =>{
+    if(result === true){
+        screenEl.innerText = ''
+        result = false
+    }
+    screenEl.innerText = screenEl.innerText + btn.innerText;
+  });
+}
+
+function calculate(){
+    console.log(eval(screenEl.innerText))
+    console.log(result)
+    screenEl.innerText = eval(screenEl.innerText)
+    result = true
+}
